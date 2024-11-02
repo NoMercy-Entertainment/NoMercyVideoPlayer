ed3c8568198e3c66efbfaf4b7bd994e769a8350d
Stoney Eagle <45034970+StoneyEagle@users.noreply.github.com>
2024-11-02 15:46:48 +0100

    feat: chapter navigation

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,30 +9,28 @@ declare module '*.svg';
 declare module '*.png';
 declare module '*.gif';
 
-global {
-	interface Window {
-		octopusInstance: any;
-		Hls: import('hls.js');
-		gainNode: GainNode;
-		nmplayer: (id?: string) => import('./index').NMPlayer;
-		instances: Map<string, NMPlayer>;
-	}
+interface Window {
+	octopusInstance: any;
+	Hls: import('hls.js');
+	gainNode: GainNode;
+	nmplayer: (id?: string) => import('./index').NMPlayer;
+	instances: Map<string, NMPlayer>;
+}
 
-	interface Navigator {
-		deviceMemory: number;
-	}
+interface Navigator {
+	deviceMemory: number;
+}
 
-	interface Date {
-		format: any;
-	}
+interface Date {
+	format: any;
+}
 
-	interface String {
-		capitalize: () => string;
-		toPascalCase: (string) => string;
-		titleCase: (lang: string, withLowers: boolean) => string;
-		toTitleCase: (lang?: string) => string;
+interface String {
+	capitalize: () => string;
+	toPascalCase: (string) => string;
+	titleCase: (lang: string, withLowers: boolean) => string;
+	toTitleCase: (lang?: string) => string;
 
-	}
 }
 
 declare let window: Window;
